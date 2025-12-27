b696058afdedebcc6beb1aecbbb2bd39c61619d3
nikgrinding <nikgrinding@gmail.com>
2025-12-27 18:11:08 +0530

    Connect with database and install nodemon for development

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db/connect");
 
 const app = express();
 
@@ -7,7 +8,18 @@ app.get("/", (req, res) => {
     return res.status(200).send("Task Manager API is running.");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Backend is running at http://localhost:${PORT}`);
-});
+const startApplication = async () => {
+    const connectionString = process.env.MONGO_URI;
+    try {
+        await connectDB(connectionString);
+        console.log("Successfully connected to DB.");
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Backend is running at http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log("Failed to connect to DB.");
+    }
+};
+
+startApplication();
