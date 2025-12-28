d806d410fae497728e65821b756fc2815b2887f4
nikgrinding <nikgrinding@gmail.com>
2025-12-28 20:06:39 +0530

    LoadingMessage component

---
+++ frontend/src/components/ui/LoadingMessage.jsx
@@ -0,0 +1,7 @@
+export default function LoadingMessage() {
+    return (
+        <div className="flex justify-center items-center py-8">
+            <div className="text-gray-600 text-lg">Loading...</div>
+        </div>
+    );
+}
