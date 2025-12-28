accb838a055a95e335c60ddade35f00b3521459d
nikgrinding <nikgrinding@gmail.com>
2025-12-28 20:10:46 +0530

    PageContainer component

---
+++ frontend/src/components/layout/PageContainer.jsx
@@ -0,0 +1,7 @@
+export default function PageContainer({ children }) {
+    return (
+        <div className="min-h-screen bg-gray-50 py-8">
+            <div className="max-w-4xl mx-auto px-4">{children}</div>
+        </div>
+    );
+}
