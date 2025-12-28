0ecf8aa7eaa316837c74ea51cdde5fd51f0028d1
nikgrinding <nikgrinding@gmail.com>
2025-12-28 23:50:50 +0530

    Add NotFoundPage and change PageContainer width

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EditPage from "./pages/EditPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/edit/:id" element={<EditPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/components/layout/PageContainer.jsx
+++ frontend/src/components/layout/PageContainer.jsx
@@ -1,7 +1,7 @@
 export default function PageContainer({ children }) {
     return (
         <div className="min-h-screen bg-gray-50 py-8">
-            <div className="max-w-xl mx-auto px-4">{children}</div>
+            <div className="max-w-3xl mx-auto px-4">{children}</div>
         </div>
     );
 }
